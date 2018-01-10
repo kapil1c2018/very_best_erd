08b98b60dc59f5a06f52c497a70aa0262958e4f9
Raghu Betina <raghu@firstdraft.com>
2018-01-10 06:10:13 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "bookmarks#index"
   devise_for :users
   # Routes for the User resource:
